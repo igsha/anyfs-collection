9da3029a5fc5593666cafe83ac5b3aac48f41480
Igor Sharonov <igsha@users.noreply.github.com>
2025-01-02 19:08:07 +0300

    Add flake

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "A collection of anyfs structure program";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    anyfs = {
+      url = "github:igsha/anyfs";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, anyfs }: let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  in {
+    packages.x86_64-linux = rec {
+      anyfs-collection = pkgs.callPackage ./default.nix { };
+      default = anyfs-collection;
+    };
+    devShells.x86_64-linux.default = with pkgs; mkShell {
+      nativeBuildInputs = [
+        python3
+        anyfs.packages.${pkgs.system}.anyfs
+      ];
+      PYTHONDONTWRITEBYTECODE = 1;
+    };
+  };
+}
