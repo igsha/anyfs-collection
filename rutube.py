7e7d9f5c66ecb3d9b3fc2d2e910b263c169aaab8
Igor Sharonov <igsha@users.noreply.github.com>
2025-02-16 22:56:13 +0300

    rutube: Support timestamps

--- rutube.py
+++ rutube.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
 import argparse
+import datetime
 import json
-import m3u8
 import os
 import re
 import sys
@@ -23,35 +23,46 @@ class Fetcher:
         self.idmap = {}
 
     @staticmethod
-    def _printbytes(path, buf):
-        print("bytes", len(buf.encode()), path)
+    def _datetots(datets):
+        return int(datetime.datetime.fromisoformat(datets).timestamp())
+
+    @staticmethod
+    def _printbytes(path, buf, ts=None):
+        if ts is None:
+            print("bytes", len(buf.encode()), path)
+        else:
+            print("tbytes", ts, len(buf.encode()), path)
+
         sys.stdout.write(buf)
 
     @staticmethod
-    def _printentity(path):
-        print("entity", path)
+    def _printentity(path, ts=None):
+        if ts is None:
+            print("entity", path)
+        else:
+            print("tentity", ts, path)
 
     @staticmethod
-    def _printurl(path, url):
-        print("url", 0, path)
+    def _printurl(path, url, ts):
+        print("turl", ts, 0, path)
         print(url)
 
     @staticmethod
-    def _printjson(path, data):
-        Fetcher._printbytes(path, json.dumps(data, ensure_ascii=False, indent=2))
+    def _printjson(path, data, ts=None):
+        Fetcher._printbytes(path, json.dumps(data, ensure_ascii=False, indent=2), ts)
 
     @staticmethod
-    def _printlink(path, realpath):
-        print("link", path)
+    def _printlink(path, realpath, ts):
+        print("tlink", ts, path)
         print(realpath)
 
     def fetch(self, path):
         try:
             if path == "/":
                 self._printroot()
-            elif m := re.match(r"/hashes/(\w{32})/video", path):
-                self._printoptions(path, m[1])
-            elif m := re.match(r"/hashes/(\d+)/videos", path):
+            elif m := re.match(r"/\.hashes/(\w{32})/(video\.m3u8|\.info\.json)", path):
+                self._printoptions(os.path.dirname(path), m[1])
+            elif m := re.match(r"/\.hashes/(\d+)/videos", path):
                 self._printplaylist(path, m[1])
             elif re.match(r"/[^/]+(/next)*$", path):
                 pagenum = path.count("/next") + 1
@@ -73,30 +84,32 @@ class Fetcher:
         with urlopen(self._PROFILE) as f:
             data = json.load(f)
 
-        for x in ["", "videos", "playlists", "shorts", "hashes"]:
-            self._printentity("/" + x)
+        ts = self._datetots(data["date_joined"])
+        for x in ["", "videos", "playlists", "shorts", ".hashes"]:
+            self._printentity("/" + x, ts)
 
-        self._printbytes(f"/{data['name']}.txt", data["description"])
-        self._printjson("/info.json", data)
+        self._printbytes(f"/{data['name']}.txt", data["description"], ts)
+        self._printjson("/.info.json", data, ts)
 
-    def _printthumbnail(self, path, data):
+    def _printthumbnail(self, path, data, ts=None):
         thumbnail_url = data["thumbnail_url"]
         name = "thumbnail." + thumbnail_url.split(".")[-1]
-        self._printurl(os.path.join(path, name), thumbnail_url)
+        self._printurl(os.path.join(path, name), thumbnail_url, ts)
 
     def _printvideo(self, path, data):
-        self._printbytes(path + "/video.url", data["video_url"])
-        self._printbytes(path + "/about.txt", data["description"])
-        self._printthumbnail(path, data)
-        self._printentity(path + "/video")
+        pubts = self._datetots(data["publication_ts"])
+        self._printbytes(path + "/origin.m3u8", data["video_url"], pubts)
+        self._printbytes(path + "/about.txt", data["description"], pubts)
+        self._printthumbnail(path, data, pubts)
+        self._printentity(path + "/video.m3u8", pubts)
+        self._printentity(path + "/.info.json", pubts)
 
     def _printoptions(self, path, videoid):
         with urlopen(self._PLAYOPTIONS.format(videoid)) as f:
             data = json.load(f)
 
-        mplay = m3u8.load(data["video_balancer"]["m3u8"])
-        for name, url in {str(p.stream_info): p.absolute_uri for p in mplay.playlists}.items():
-            self._printbytes(f"{path}/{name}.m3u8", url)
+        self._printjson(path + "/.info.json", data)
+        self._printbytes(path + "/video.m3u8", data["video_balancer"]["m3u8"])
 
     def _printcommon(self, url, path):
         with urlopen(url) as f:
@@ -104,20 +117,24 @@ class Fetcher:
 
         for val in data["results"]:
             title = val["title"].replace("/", ",")
-            ppath = "/hashes/" + str(val["id"])
-            self._printlink(os.path.join(path, title), ppath)
-            self._printentity(ppath)
+            ppath = "/.hashes/" + str(val["id"])
+            if "publication_ts" in val:
+                ts = self._datetots(val["publication_ts"])
+            else:
+                ts = self._datetots(val["created_ts"])
+
+            self._printlink(os.path.join(path, title), ppath, ts)
             if "videos_count" in val:
-                self._printthumbnail(ppath, val)
-                self._printbytes(ppath + "/playlist.url", f"{self.BASEURL}/plst/{val['id']}/")
-                self._printentity(ppath + "/videos")
+                self._printthumbnail(ppath, val, ts)
+                self._printbytes(ppath + "/playlist.m3u8", f"{self.BASEURL}/plst/{val['id']}/", ts)
+                self._printentity(ppath + "/videos", ts)
             else:
                 self._printvideo(ppath, val)
 
         if data["has_next"]:
             self._printentity(path + "/next")
 
-        self._printjson(path + "/info.json", data)
+        self._printjson(path + "/.info.json", data)
 
     def _printplaylist(self, path, playlistid):
         self._printcommon(f"{self.BASEURL}/api/playlist/custom/{playlistid}/videos/", path)
